28c73f61b5f7d10a65d6f47cb7c9fa8eba05e3ee
oguz yurken <oguz.yurken@gmail.com>
2022-11-06 22:56:15 +0300

    Adjust LocationPicker

--- src/components/Places/LocationPicker.js
+++ src/components/Places/LocationPicker.js
@@ -1,12 +1,40 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import OutlinedButton from '../UI/OutlinedButton';
+import { Colors } from '../../constants/colors';
 
 export default function LocationPicker() {
+  const getLocationHandler = () => {};
+  const pickOnMapHandler = () => {};
+
   return (
     <View>
-      <Text>LocationPicker</Text>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
