1eb06176bb30038d563def2d717baa4be2672624
oguz yurken <oguz.yurken@gmail.com>
2022-11-06 22:49:43 +0300

    Create LocationPicker

---
+++ src/components/Places/LocationPicker.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+export default function LocationPicker() {
+  return (
+    <View>
+      <Text>LocationPicker</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
