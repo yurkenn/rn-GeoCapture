f8a0cda3d98582a7f32b940fa759227adde92b30
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 08:53:36 +0300

    Update Map.js

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useLayoutEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-const Map = () => {
+import IconButton from '../components/ui/IconButton';
+const Map = ({ navigation }) => {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const mapRegion = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={mapRegion}></MapView>;
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({
+      lat: lat,
+      lng: lng,
+    });
+  };
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert('No location picked!', 'Please pick a location (by tapping on map) first.', [
+        { text: 'Okay' },
+      ]);
+      return;
+    }
+    navigation.navigate('AddPlace', {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton icon={'save'} color={tintColor} size={24} onPress={savePickedLocationHandler} />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
+
+  return (
+    <MapView onPress={selectLocationHandler} style={styles.map} initialRegion={mapRegion}>
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{ latitude: selectedLocation.lat, longitude: selectedLocation.lng }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 export default Map;
