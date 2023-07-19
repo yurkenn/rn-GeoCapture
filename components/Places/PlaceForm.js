f3095e7433c34821a24bd3b2ba96c773e7bbf762
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 03:32:56 +0300

    Create PlaceForm.js

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,43 @@
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Colors } from '../../constants/colors';
+
+const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  const changeTitleHandler = (enteredText) => {
+    setEnteredTitle(enteredText);
+  };
+
+  return (
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enteredTitle} />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
