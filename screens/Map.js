83252ffade941645681fe128f85a43c483cbede2
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 05:04:35 +0300

    Update Map.js

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-
+import MapView, { Marker } from 'react-native-maps';
 const Map = () => {
-  return (
-    <View>
-      <Text>Map</Text>
-    </View>
-  );
+  const mapRegion = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+  return <MapView style={styles.map} initialRegion={mapRegion}></MapView>;
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
