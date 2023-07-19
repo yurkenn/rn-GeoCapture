40b80b2b6b601b03e29a652bdb2ecffd1db1bfb7
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 03:33:06 +0300

    Create place.js

---
+++ models/place.js
@@ -0,0 +1,9 @@
+class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location; // { lat: ..., lng: ... }
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}
