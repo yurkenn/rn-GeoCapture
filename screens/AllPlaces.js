414ca151b49f198713e187d64c06b955e2acc1d1
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 03:33:14 +0300

    Create AllPlaces.js

---
+++ screens/AllPlaces.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import PlacesList from '../components/Places/PlacesList';
+
+const AllPlaces = () => {
+  return <PlacesList />;
+};
+
+export default AllPlaces;
+
+const styles = StyleSheet.create({});
