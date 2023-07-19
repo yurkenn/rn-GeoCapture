64eda61a21dfc06f228d7c67e87d6c8736bdbe57
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 03:32:17 +0300

    Delete all and start over

--- src/models/place.js
+++
@@ -1,9 +0,0 @@
-class Place {
-  constructor(title, imageUri, address, location) {
-    this.title = title;
-    this.imageUri = imageUri;
-    this.address = address;
-    this.location = location;
-    this.id = new Date().toString() + Math.random().toString();
-  }
-}
