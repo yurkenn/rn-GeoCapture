a019faf013f6662824a499f69ec4b5b127c8c734
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-19 03:33:18 +0300

    Create PlaceDetail.js

---
+++ screens/PlaceDetail.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const PlaceDetail = () => {
+  return (
+    <View>
+      <Text>PlaceDetail</Text>
+    </View>
+  );
+};
+
+export default PlaceDetail;
+
+const styles = StyleSheet.create({});
