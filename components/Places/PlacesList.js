d93267a93fc0cd6ad6b9a12b7a667d83a7c41457
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-23 23:26:01 +0300

    Update PlacesList.js

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -2,8 +2,10 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import PlaceItem from './PlaceItem';
 import { Colors } from '../../constants/colors';
+import { useNavigation } from '@react-navigation/native';
 
 const PlacesList = ({ places }) => {
+  const navigation = useNavigation();
   if (!places || places.length === 0)
     return (
       <View style={styles.fallbackContainer}>
@@ -11,14 +13,30 @@ const PlacesList = ({ places }) => {
       </View>
     );
 
-  const renderItem = ({ item }) => <PlaceItem place={item} />;
+  const selectPlaceHandler = (id) => {
+    navigation.navigate('PlaceDetail', {
+      placeId: id,
+    });
+  };
 
-  return <FlatList data={places} keyExtractor={(item) => item.id} renderItem={renderItem} />;
+  const renderItem = ({ item }) => <PlaceItem place={item} onSelect={selectPlaceHandler} />;
+
+  return (
+    <FlatList
+      style={styles.list}
+      data={places}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderItem}
+    />
+  );
 };
 
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
     justifyContent: 'center',
