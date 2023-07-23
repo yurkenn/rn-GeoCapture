96e0ab4abf7cf057e073ee9122d635b011f1d76b
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-23 23:26:12 +0300

    Update AllPlaces.js

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,9 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PlacesList from '../components/Places/PlacesList';
+import { useIsFocused } from '@react-navigation/native';
+import { fetchPlaces } from '../util/database';
 
-const AllPlaces = () => {
-  return <PlacesList />;
+const AllPlaces = ({ route }) => {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
+    }
+    if (isFocused) {
+      loadPlaces();
+    }
+  }, [isFocused]);
+
+  return <PlacesList places={loadedPlaces} />;
 };
 
 export default AllPlaces;
