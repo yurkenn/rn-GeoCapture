be5ff45374dfa6bdba4c3e3473d7d304aae42b39
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-23 23:26:17 +0300

    Update PlaceDetail.js

--- screens/PlaceDetail.js
+++ screens/PlaceDetail.js
@@ -1,14 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import OutlinedButton from '../components/ui/OutlinedButton';
+import { Colors } from '../constants/colors';
+import { fetchPlaceDetails } from '../util/database';
 
-const PlaceDetail = () => {
+const PlaceDetail = ({ route, navigation }) => {
+  const [fetchedPlace, setFetchedPlace] = useState();
+
+  const showOnMapHandler = () => {
+    navigation.navigate('Map', {
+      initialLat: fetchedPlace.location.lat,
+      initialLng: fetchedPlace.location.lng,
+    });
+  };
+
+  const selectedPlaceId = route.params.placeId;
+
+  useEffect(() => {
+    async function loadPlaceData() {
+      const place = await fetchPlaceDetails(selectedPlaceId);
+      setFetchedPlace(place);
+      navigation.setOptions({ title: place.title });
+    }
+    loadPlaceData();
+  }, [selectedPlaceId]);
+
+  if (!fetchedPlace) {
+    return (
+      <View style={styles.fallback}>
+        <Text>Loading place data...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>PlaceDetail</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: fetchedPlace.imageUri }} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{fetchedPlace.address}</Text>
+        </View>
+        <OutlinedButton icon={'map'} onPress={showOnMapHandler}>
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceDetail;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  fallback: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  screen: {
+    alignItems: 'center',
+  },
+  image: {
+    height: '35%',
+    minHeight: 300,
+    width: '100%',
+  },
+  locationContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: Colors.primary500,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
