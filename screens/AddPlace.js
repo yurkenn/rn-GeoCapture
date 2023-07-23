29cbac8f93b0a7fb746df87190d2baacee3399a8
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-23 23:26:10 +0300

    Update AddPlace.js

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
 import PlaceForm from '../components/Places/PlaceForm';
+import { insertPlace } from '../util/database';
 
 const AddPlace = ({ navigation }) => {
-  const createPlaceHandler = (title, image) => {};
-  return <PlaceForm />;
+  const createPlaceHandler = async (place) => {
+    await insertPlace(place);
+    navigation.navigate('AllPlaces');
+  };
+
+  return <PlaceForm onCreatePlace={createPlaceHandler} />;
 };
 
 export default AddPlace;
-
-const styles = StyleSheet.create({});
